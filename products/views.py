734528fd467d971eb04578e7665caf4bcd86fd40
Muhammed Recep Hava <ragab5434@gmail.com>
2024-01-25 03:37:14 +0300

    show product reviews from views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
@@ -12,4 +13,9 @@ class ProductList(ListView):
 
 
 class ProductDetail(DetailView):
-    model = Product
+    model = Product 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = Review.objects.filter(product=self.get_object()) # self.get_object() ==> return the object that we are working with now
+        return context
