4604967300fb30cfe70696419fd77bb7b9af1a73
Muhammed Recep Hava <ragab5434@gmail.com>
2024-01-26 07:27:06 +0300

    add brand list & detail pages with their functionality

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ProductDetail, ProductList
+from .views import ProductDetail, ProductList, BrandList, BrandDetail
 
 
 urlpatterns = [
     path('', ProductList.as_view(), name='product_list'),
+    path('brands/', BrandList.as_view(), name='brand_list'),
+    path('brands/<slug:slug>/', BrandDetail.as_view(), name='brand_detail'),
     path("<slug:slug>/", ProductDetail.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
@@ -20,4 +21,37 @@ class ProductDetail(DetailView):
         context['reviews'] = Review.objects.filter(product=self.get_object()) # self.get_object() ==> return the object that we are working with now
         context['images'] = ProductImages.objects.filter(product=self.get_object())
         context['related'] = Product.objects.filter(brand=self.get_object().brand)[:10]
+        return context
+
+
+
+class BrandList(ListView):
+    model = Brand
+
+
+
+# class BrandDetail(DetailView):
+#     model = Brand
+
+#     def get_context_data(self, **kwargs) -> dict[str, Any]:
+#         context = super().get_context_data(**kwargs)
+#         context["products"] = Product.objects.filter(brand=self.get_object())
+#         return context
+
+
+
+class BrandDetail(ListView):
+    model = Product
+    template_name = 'products/brand_detail.html'
+
+    def get_queryset(self):
+        brand = Brand.objects.get(slug=self.kwargs['slug'])
+        queryset = super().get_queryset().filter(brand=brand)
+
+        return queryset
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['brand'] = Brand.objects.get(slug=self.kwargs['slug'])
+
         return context
