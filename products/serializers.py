9a361a7093ce567c79e5393553b39f02a22278af
Muhammed Recep Hava <ragab5434@gmail.com>
2024-01-26 08:52:40 +0300

    add product (list & detail) and brand (list & detail) api

---
+++ products/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+
+from .models import Brand, Product
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+
+class BrandListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Brand
+        fields = "__all__"
+
+
+
+class BrandDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Brand
+        fields = "__all__"
--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from .views import ProductDetail, ProductList, BrandList, BrandDetail
 
+from . import api
+
 
 urlpatterns = [
     path('', ProductList.as_view(), name='product_list'),
     path('brands/', BrandList.as_view(), name='brand_list'),
     path('brands/<slug:slug>/', BrandDetail.as_view(), name='brand_detail'),
     path("<slug:slug>/", ProductDetail.as_view(), name='product_detail'),
+
+    # API urls
+    path('api/list', api.ProductListAPI.as_view(), name='product_list_api'),
+    path('api/list/brands/', api.BrandListAPI.as_view(), name='brand_list_api'),
+    path('api/list/brands/<int:pk>/', api.BrandDetailAPI.as_view(), name='brand_detail_api'),
+    path('api/list/<int:pk>/', api.ProductDetailAPI.as_view(), name='product_detail_api'),
 ]
