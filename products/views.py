fca044c5c53e4df2d9784ff6908f61cd5d40b510
Muhammed Recep Hava <ragab5434@gmail.com>
2024-01-27 22:48:50 +0300

    add product count of brand in brand detail page

--- products/views.py
+++ products/views.py
@@ -145,6 +145,6 @@ class BrandDetail(ListView):
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context['brand'] = Brand.objects.get(slug=self.kwargs['slug'])
+        context['brand'] = Brand.objects.filter(slug=self.kwargs['slug']).annotate(product_count=Count('product_brand'))[0]
 
         return context
