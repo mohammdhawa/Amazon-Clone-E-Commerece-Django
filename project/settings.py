4e4591d5e83076750c80f2f6d66c4c11b7638a2f
Muhammed Recep Hava <ragab5434@gmail.com>
2024-01-28 22:02:33 +0300

    add home template, clean navbar

--- project/settings.py
+++ project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
     # My Apps
     "products",
+    "settings",
 ]
 
 
--- project/urls.py
+++ project/urls.py
@@ -26,10 +26,12 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [ 
+    path('', include('settings.urls')),
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
 
-path("__debug__/", include("debug_toolbar.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+    
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
