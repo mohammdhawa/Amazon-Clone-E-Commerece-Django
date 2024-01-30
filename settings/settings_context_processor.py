5b9e9070c6fd5729945e6030abdce7218e50dcaa
Muhammed Recep Hava <ragab5434@gmail.com>
2024-01-30 21:56:07 +0300

    add chaching for settings_context_processor

--- settings/settings_context_processor.py
+++ settings/settings_context_processor.py
@@ -1,7 +1,22 @@
 from .models import Settings
 
+from django.core.cache import cache
+
 
 def get_settings(request):
-    data = Settings.objects.last()
     
-    return {"settings_data": data}
+
+    # check data in cache
+    try:
+        settings_data = cache.get('settings_data')
+        print()
+        print("*"*50, end='\n')
+        print("Before any exceptino", settings_data)
+    except Exception:
+        print()
+        print("*"*50, end='\n')
+        print("An exception Error: ")
+        settings_data = Settings.objects.last()
+        cache.set('settings_data', settings_data, 60*60*24*30)
+    
+    return {"settings_data": settings_data}
