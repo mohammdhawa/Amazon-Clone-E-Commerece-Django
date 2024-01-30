d9c59a3679de6ddd78934a8dd8eabc43527ba21c
Muhammed Recep Hava <ragab5434@gmail.com>
2024-01-30 23:08:06 +0300

    show brands in home page

--- settings/views.py
+++ settings/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+from products.models import Product, Brand, Review
+
+from django.db.models.aggregates import Count
+
 # Create your views here.
 
 def home(request):
-
-    return render(request, 'settings\home.html', {})
+    new_products = Product.objects.filter(flag='NEW')[:10]
+    sale_products = Product.objects.filter(flag='SALE')[:10]
+    feature_products = Product.objects.filter(flag='FEATURE')[:6]
+    brands = Brand.objects.annotate(product_count=Count('product_brand'))[:10]
+    reviews = Review.objects.all()[:6]
+    
+    return render(request, 'settings\home.html', {
+        'new_products': new_products,
+        'sale_products': sale_products,
+        'feature_products': feature_products,
+        'brands': brands,
+        'reviews': reviews
+    })
