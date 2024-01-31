7a641f19e854e41f865203b4b832e1cd0b4be95b
Muhammed Recep Hava <ragab5434@gmail.com>
2024-01-31 21:48:06 +0300

    fix settings context processor

--- settings/settings_context_processor.py
+++ settings/settings_context_processor.py
@@ -4,19 +4,20 @@ from django.core.cache import cache
 
 
 def get_settings(request):
-    
-
-    # check data in cache
-    try:
-        settings_data = cache.get('settings_data')
-        print()
-        print("*"*50, end='\n')
-        print("Before any exceptino", settings_data)
-    except Exception:
-        print()
-        print("*"*50, end='\n')
-        print("An exception Error: ")
-        settings_data = Settings.objects.last()
-        cache.set('settings_data', settings_data, 60*60*24*30)
+    # Check data in cache
+    # try:
+    #     settings_data = cache.get('settings_data')
+    #     print("*" * 50, end='\n')
+    #     print("Cache hit:", settings_data)
+    # except cache.DoesNotExist:
+    #     print("*" * 50, end='\n')
+    #     print("Cache miss: Exception caught")
+    #     try:
+    #         settings_data = Settings.objects.last()
+    #         cache.set('settings_data', settings_data, 60*60*24*30)
+    #     except Exception as e:
+    #         print(f"An exception occurred while retrieving settings from the database: {e}")
+    #         settings_data = None
+    settings_data = Settings.objects.last()
     
     return {"settings_data": settings_data}
--- settings/views.py
+++ settings/views.py
@@ -15,11 +15,6 @@ def home(request):
     brands = Brand.objects.annotate(product_count=Count('product_brand'))[:10]
     reviews = Review.objects.all()[:6]
     settings_data = Settings.objects.last()
-
-    print("\n\n*********************************\n")
-    for r in reviews:
-        print(r.rate)
-    print("\n\n*********************************\n")
     
     return render(request, 'settings\home.html', {
         'new_products': new_products,
